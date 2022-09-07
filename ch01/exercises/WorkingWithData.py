3fddc65e33ddec53f208270fda1268a625c9531f
Lucas Diaz <112440062+ldiaz16@users.noreply.github.com>
2022-09-07 21:27:10 +0000

    I made the output much cleaner by separating cents and dollars aswell as making sure that there aren't an excessive amount of digits in the decimal

--- ch01/exercises/WorkingWithData.py
+++ ch01/exercises/WorkingWithData.py
@@ -12,9 +12,25 @@ print(10/15)
 
 rate = float(input("What is the current exchange rate for 1 Euro to Dollars? "))
 print("The exchange rate is 1 Euro to ",rate,"dollars")
+
 amount = float(input("How many Euros are you exchanging? "))
 print("You have entered", amount, "Euros")
+
 total = rate * amount
 total = total - 3
-print("After the service fee has been applied, you have", total,"dollars")
+
+strTotal = str(total)
+sDot = strTotal.index('.')
+
+for i in range(len(strTotal)):
+  if i == sDot+3:
+    strTotal = strTotal[0:i]
+
+strCents = strTotal[sDot+1:sDot+3]
+strDollars = strTotal[0:sDot]
+
+dollars = int(strDollars)
+cents = int(strCents)
+
+print("After the service fee has been applied, you have", dollars,"dollars and",cents,"Cents")
      
