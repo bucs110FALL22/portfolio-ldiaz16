b514751b1f65523eb354026aab7f35162f3c7afa
Lucas Diaz <112440062+ldiaz16@users.noreply.github.com>
2022-09-08 23:04:47 +0000

    I completed Lab 2 by adding a random list of strings and generating a random result from said list

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,3 +1,4 @@
+import random
 
 #Part A
 weeks = 16
@@ -6,5 +7,13 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 3
+print(classes_per_week,type(classes_per_week))
 
+cost_per_class = cost_per_week/classes_per_week
+print(cost_per_class,type(cost_per_class))
+print("The cost per class is",cost_per_class)
 #Part B
+listRandom = ["Boeing",'Airbus','Embraer','Bombardier','Cessna']
+random_airplane = random.choice(listRandom)
+print("The random airplane is a(n)",random_airplane)
